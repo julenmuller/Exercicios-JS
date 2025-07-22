dd9fdf77dfcf34ace9034967fd73b0f69fa93c11
julenmuller <julianamullergimenes@gmail.com>
2025-07-22 20:41:37 -0300

    exercicios de JS da semana

---
+++ exercicio-2/segundo.js
@@ -0,0 +1,7 @@
+const boletoPago = false;
+
+if (boletoPago) {
+    alert('Boleto foi pago com sucesso!')
+} else if (boletoPago === false) {
+    alert('Boleto não foi pago!')
+}
---
+++ exercicio-3/terceiro.js
@@ -0,0 +1,9 @@
+const vaiSeHospedar = confirm('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem? ')
+
+console.log(vaiSeHospedar)
+
+if(vaiSeHospedar){
+    alert('Ótimo! Nós temos as melhores camas de toda a região!')
+} else {
+     alert('Que pena! Você parecia ser uma pessoa mais legal')
+}
